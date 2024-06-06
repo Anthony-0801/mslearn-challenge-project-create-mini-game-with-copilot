ad298e1307b98f4f7e29cc4c4330b6cee8a92abf
Anthony <141275535+Anthony-0801@users.noreply.github.com>
2024-06-06 05:09:48 +0000

    adding details

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def clear():
 def game():
     clear()
     print("Welcome to Rock, Paper, Scissors")
+    print("Please enter the number of your choice:")
     print("1. Rock")
     print("2. Paper")
     print("3. Scissors")
@@ -16,11 +17,10 @@ def game():
     computer = random.randint(1,3)
 
     #Add validation that when the user enter a string or a number that is not 1, 2 or 3, the program should print an error message and ask the user to enter a valid number
-    if player not in [1, 2, 3]:
+    if player != 1 and player != 2 and player != 3:
         print("Invalid choice!")
         game()
-        return
-    
+
     if player == computer:
         print("It's a tie!")
     elif player == 1:
